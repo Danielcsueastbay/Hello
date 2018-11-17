1a513d114cdfc8048af1553138880709ef2c22f1
Daniel <Daniel@DESKTOP-S015Q0T>
2018-11-16 20:22:44 -0800

    linear search test

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,18 +5,47 @@
  */
 package hello;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 /**
  *
  * @author Daniel
  */
 public class Hello {
-
     /**
      * @param args the command line arguments
      */
+    public static <T> int linearSearch(List<T> list, Predicate<T> check) {
+        for(int i = 0; i < list.size(); i++){
+            if(check.test(list.get(i)))
+                return i;
+        }     
+        return -1;
+    }
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World!");
+        ///System.out.println("Hello World!");
+        List<String> str = new ArrayList();
+        List<Integer> num = new ArrayList();
+        String sss = "Green";
+        String sss2 = "Blue";
+        int iii = 2;
+        str.add("Travis");
+        str.add("Red");
+        str.add("Blue");
+        num.add(2);
+        num.add(6);
+        num.add(7);
+        Hello h = new Hello();
+        int found;
+        found = h.linearSearch(str, s -> s.equals(sss));    //not found
+        System.out.println("Index " + found);
+        found = h.linearSearch(num, i -> i.equals(iii));    //found at position 0
+        System.out.println(found);
+        found = h.linearSearch(str, s -> s.equals(sss2));    //found at position 2
+        System.out.println(found);
     }
     
 }
